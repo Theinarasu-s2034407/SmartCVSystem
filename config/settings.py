fd750ad27cec7e7b09232f95bd915774271909d8
ザティル・イマニ <zatilimani.work@gmail.com>
2025-06-17 22:16:40 +0800

    remove aws

--- config/settings.py
+++ config/settings.py
@@ -133,12 +133,6 @@ DATABASES = {
 }
 
 
-INSTALLED_APPS += ["storages"]
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
-AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "")
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 # if not DEBUG:
 #     INSTALLED_APPS += ["storages"]
